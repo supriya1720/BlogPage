44e8f73632278ffa2eebef6e4a8d38479459419d
Supriya Singh <supriyasingh@Supriyas-MacBook-Air.local>
2025-05-31 21:49:33 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/BlogPage/',  
   plugins: [react()],
-  base: '/', // crucial for Vercel
 })
