773f40e5d8be61c41b564b06b2d83ff01441aaf5
Supriya Singh <supriyasingh@Supriyas-MacBook-Air.local>
2025-05-31 20:33:03 +0530

    modules modified

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,12 @@
 const info=[
     {
-        title:"Supriya",
+        title:"Smriti",
+        content:"Art is a diverse range of human activities involving the creation of visual, auditory, or performance artifacts that express the creator's imagination, conceptual ideas, or technical skill. It is valued for its beauty, emotional power, and capacity to communicate complex themes and ideas.",
+        theme:"Art",
+        description:"My art experience. ",
+    },
+    {
+        title:"Aniket",
         content:"Traveling opens your mind to new cultures, experiences, and perspectives, enriching your life in countless ways. It's a chance to explore the world, create unforgettable memories, and grow as an individual.",
         theme:"Travel",
         description:'My travelling experience.',
@@ -12,12 +18,6 @@ const info=[
         theme:"Technology",
         description:"My technology experience.",
 
-    },
-    {
-        title:"Tanya",
-        content:"Art is a diverse range of human activities involving the creation of visual, auditory, or performance artifacts that express the creator's imagination, conceptual ideas, or technical skill. It is valued for its beauty, emotional power, and capacity to communicate complex themes and ideas.",
-        theme:"Art",
-        description:"My art experience. ",
     },
 ]
 export default info;
